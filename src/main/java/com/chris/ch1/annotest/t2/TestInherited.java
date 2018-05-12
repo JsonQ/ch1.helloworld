4f42da27c74f73e1a34a1a0f68744922fd60762d
秦亮 <qmzj100@gmail.com>
2018-05-12 22:57:37 +0800

    Annotation Test

--- src/main/java/com/chris/ch1/annotest/t2/TestInherited.java
+++ src/main/java/com/chris/ch1/annotest/t2/TestInherited.java
@@ -4,5 +4,8 @@ public class TestInherited {
 
     public static void main(String[] args){
 
+        Class<AnnoChildObj> annoChild = AnnoChildObj.class;
+        Anno anno = annoChild.getAnnotation(Anno.class);
+        System.out.println("annoation.name:" + anno.name());
     }
 }
